573043b7b230ff65549ed96d7c2a8bb1221e618e
denimendica <71275614+denimendica@users.noreply.github.com>
2020-11-22 12:50:15 +0100

    Add files via upload

---
+++ string4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sVelika, sMala, sPrvaTri, sZadnjihPet, s8_11;
+
+
+            Console.Write("Unesite niz znakova: ");
+            string nizZnakova = Console.ReadLine();
+
+            Console.WriteLine(nizZnakova.ToUpper());
+            Console.WriteLine(nizZnakova.ToLower());
+            Console.WriteLine(nizZnakova.Substring(0, 3));
+            Console.WriteLine(nizZnakova.Substring(7, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
